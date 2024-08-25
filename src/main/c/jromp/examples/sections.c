f668861b0cd94f35eaead8ba025ba97db4b1f39a
scastd <scastd00@gmail.com>
2024-08-25 17:56:24 +0200

    Normalize indentation in sections example.
    Adjusted the code by replacing tabs with spaces to ensure consistent indentation. This change improves readability and maintains a uniform coding style across the file.

--- src/main/c/jromp/examples/sections.c
+++ src/main/c/jromp/examples/sections.c
@@ -4,31 +4,31 @@
 int main(int argc, char *argv[]) {
     #pragma omp parallel sections num_threads(3)
     {
-		#pragma omp section
-		{
-			printf("Task 1. Running thread %d\n", omp_get_thread_num());
-		}
-
-		#pragma omp section
-		{
-			printf("Task 2. Running thread %d\n", omp_get_thread_num());
-		}
-
-		#pragma omp section
-		{
-			printf("Task 3. Running thread %d\n", omp_get_thread_num());
-		}
+        #pragma omp section
+        {
+            printf("Task 1. Running thread %d\n", omp_get_thread_num());
+        }
 
         #pragma omp section
-		{
-			printf("Task 4. Running thread %d\n", omp_get_thread_num());
-		}
+        {
+            printf("Task 2. Running thread %d\n", omp_get_thread_num());
+        }
 
         #pragma omp section
-		{
-			printf("Task 5. Running thread %d\n", omp_get_thread_num());
-		}
-	} // Implicit barrier
+        {
+            printf("Task 3. Running thread %d\n", omp_get_thread_num());
+        }
+
+        #pragma omp section
+        {
+            printf("Task 4. Running thread %d\n", omp_get_thread_num());
+        }
+
+        #pragma omp section
+        {
+            printf("Task 5. Running thread %d\n", omp_get_thread_num());
+        }
+    } // Implicit barrier
 
     return 0;
 }
